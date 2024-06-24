8f52666ba670a7fce09d5f6cfb5b63b8bb52c5e2
Supriya <supriyabicte@gmail.com>
2024-06-24 06:04:04 +0545

    check

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+export default function nav() {
+  return (
+    <div class="container">
+      <img src="logo.png" />
+      <h1>L O G I N</h1>
+      <input type="text" placeholder="Username" required />
+      <input type="password" placeholder="Password" required />
+      <button type="submit">LOGIN</button>
+    </div>
+  );
+}
